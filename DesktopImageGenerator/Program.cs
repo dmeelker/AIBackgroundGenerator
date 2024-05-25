e86c05316fa1a2228002d8e5342b12bae30f8c38
Dennis Meelker <dmeelker@gmail.com>
2024-05-25 14:46:49 +0200

    Added install and uninstall commands

--- DesktopImageGenerator/Program.cs
+++ DesktopImageGenerator/Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using IWshRuntimeLibrary;
+using Microsoft.Extensions.Configuration;
 using OpenAI_API;
 using OpenAI_API.Chat;
 using OpenAI_API.Images;
 using OpenAI_API.Models;
+using System.CommandLine;
+using System.Reflection;
 
 namespace DesktopImageGenerator;
 
@@ -10,15 +13,43 @@ internal class Program
 {
     private const string _outputDirectory = "images";
 
-    static async Task Main(string[] args)
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Generates a daily background image");
+
+        var generateCommand = new Command("generate", "Generate a new desktop image");
+        var installCommand = new Command("install", "Install the application to the startup folder");
+        var uninstallCommand = new Command("uninstall", "Uninstall the application from the startup folder");
+
+        rootCommand.SetHandler(GenerateImage);
+        generateCommand.SetHandler(GenerateImage);
+        installCommand.SetHandler(InstallAutostart);
+        uninstallCommand.SetHandler(UninstallAutostart);
+
+        rootCommand.AddCommand(generateCommand);
+        rootCommand.AddCommand(installCommand);
+        rootCommand.AddCommand(uninstallCommand);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static async Task GenerateImage()
     {
         var config = LoadConfiguration();
-        var openAI = new OpenAI(config["apiKey"]!);
+        var apiKey = config["apiKey"];
+
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            Console.WriteLine("API key is missing, place your OpenAI api key in appsettings.json to get started");
+            return;
+        }
+
+        var openAI = new OpenAI(apiKey);
         var fileName = GetTodaysFileName();
 
         if (fileName.Exists)
         {
-            Console.WriteLine("Image already exists, exiting...");
+            Console.WriteLine("Today's image already exists, exiting...");
             return;
         }
 
@@ -51,7 +82,60 @@ internal class Program
     private static void SaveImage(byte[] bytes, FileInfo fileName)
     {
         Directory.CreateDirectory(_outputDirectory);
-        File.WriteAllBytes(fileName.FullName, bytes);
+        System.IO.File.WriteAllBytes(fileName.FullName, bytes);
+    }
+
+    private static void InstallAutostart()
+    {
+        var shellClass = new WshShellClass();
+        string exeFullName = GetCurrentBinary();
+        var shortcutFile = GetAutostartShortcutFileName();
+
+        if (System.IO.File.Exists(shortcutFile))
+        {
+            Console.WriteLine("Autostart already set up, exiting...");
+            return;
+        }
+
+        var shortcut = (IWshShortcut)shellClass.CreateShortcut(shortcutFile);
+        shortcut.TargetPath = exeFullName;
+        shortcut.IconLocation = exeFullName;
+        shortcut.WorkingDirectory = Path.GetDirectoryName(exeFullName);
+        shortcut.Arguments = "";
+        shortcut.Description = "Generate a desktop image";
+        shortcut.WindowStyle = 7; // Minimized
+        shortcut.Save();
+
+        Console.WriteLine("Autostart entry installed");
+    }
+
+    private static void UninstallAutostart()
+    {
+        var shortcutFile = GetAutostartShortcutFileName();
+
+        if (!System.IO.File.Exists(shortcutFile))
+        {
+            Console.WriteLine("The app has not been set to autostart");
+            return;
+        }
+
+        System.IO.File.Delete(shortcutFile);
+        Console.WriteLine("Autostart entry removed");
+    }
+
+    private static string GetAutostartShortcutFileName()
+    {
+        var startupDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        return Path.Combine(startupDirectory, "DesktopImageGenerator.lnk");
+    }
+
+    private static string GetCurrentBinary()
+    {
+        string exeFullName = Assembly.GetExecutingAssembly().Location;
+        var directory = Path.GetDirectoryName(exeFullName)!;
+        var exeName = Path.GetFileNameWithoutExtension(exeFullName) + ".exe";
+
+        return Path.Combine(directory, exeName);
     }
 
     private static IConfiguration LoadConfiguration()
